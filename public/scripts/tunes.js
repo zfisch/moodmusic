c1def4e912fd3ee002b7e2f577adcffd1f9785cc
Zack Fischmann <zacharyrobert@gmail.com>
2015-04-28 15:28:20 -0700

    update colors and add logo

--- public/scripts/tunes.js
+++ public/scripts/tunes.js
@@ -93,17 +93,21 @@ $(document).ready(function() {
   });
 
 
+  //======================================//
+  //              COLORS                  //
+  //======================================//
+
   var currentSentiment;
 
   var colors = {
     0: '#000000', //black
     1: '#eb1426', //red
     2: '#c87337', //orange meh
-    3: '#916e78', //gray-purple
+    3: '#6B0A91', //purple
     4: '#4fb0a8', //neutral blue
     5: '#484cb7', //indigo-blue
-    6: '#ee5b11', //warm-orange
-    7: '#b5f10e', //yellow-green
+    6: '#3fb5bf', //turqoise
+    7: '#ee5b11', //warm-orange
     8: '#3fb5bf', //turqoise
     9: '#eb14a3', //bright pink
     10: '#eb14a3', //bright pink
